b01f797fffce45fc39594e56f373dbfc53a12d1a
Chuck Ha <ha.chuck@gmail.com>
2013-06-20 11:25:52 -0400

    Initial Commit
    Adds info in the README
    Adds simple mgo boilerplate

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"log"
+	"labix.org/v2/mgo"
+)
+
+const (
+	mongoUrl = "localhost"
+	dbName = "squid"
+	collection = "websites"
+)
+
+var (
+	session *mgo.Session
+)
+
+func GetSession() *mgo.Session {
+	if session == nil {
+		session, err := mgo.Dial(mongoUrl)
+		if err != nil {
+			log.Printf("Error connecting to mongo: %s", err)
+		}
+	}
+	return session.Clone()
+}
+
+func GetCollection() *mgo.Collection {
+	session := GetSession()
+	return session.DB("").C(collection)
+}
+
